024dc28ee398e0ea4a75730fcd1c0caeea3f7a06
unknown <keanconsolacion@gmail.com>
2022-08-17 00:18:16 +0800

    change bg-color

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import Preview from "./Preview";
 const App = () => {
 	return (
 		<DataStore>
-			<div className="h-screen w-screen bg-[#3A4870] ">
+			<div className="h-screen w-screen bg-[#092d3b] ">
 				<h1 className="absolute bottom-0 left-0 m-4 font-medium text-white text-md drop-shadow-lg">Minutes by Ke'an</h1>
 
 				<div className="flex flex-col w-full h-full md:flex-row">
